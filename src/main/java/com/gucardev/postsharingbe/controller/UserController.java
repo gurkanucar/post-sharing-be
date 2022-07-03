58f9a3627bfc97a3b77fb236fb5b0e4c4a8087a1
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-07-03 21:03:35 +0300

    typo fixed in user controller

--- src/main/java/com/gucardev/postsharingbe/controller/UserController.java
+++ src/main/java/com/gucardev/postsharingbe/controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/post")
+@RequestMapping("/user")
 public class UserController {
 
     private final UserService userService;
