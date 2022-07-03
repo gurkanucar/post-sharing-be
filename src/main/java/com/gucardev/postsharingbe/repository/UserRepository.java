48cbd8c65f06f3b08bb372f6e57820b9182be608
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-07-03 20:17:48 +0300

    user repo created

--- src/main/java/com/gucardev/postsharingbe/repository/UserRepository.java
+++ src/main/java/com/gucardev/postsharingbe/repository/UserRepository.java
@@ -2,6 +2,13 @@ package com.gucardev.postsharingbe.repository;
 
 import com.gucardev.postsharingbe.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UserRepository extends MongoRepository<User,String> {
+
+    Optional<User> findUserByUsername(String username);
+    Optional<User> findUserById(String id);
 }
