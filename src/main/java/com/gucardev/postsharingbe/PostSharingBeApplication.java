dccdc9613434be5edc96099bf14211fb1c3cbb0d
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-07-03 18:25:15 +0300

    Initial commit

---
+++ src/main/java/com/gucardev/postsharingbe/PostSharingBeApplication.java
@@ -0,0 +1,13 @@
+package com.gucardev.postsharingbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostSharingBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostSharingBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gucardev/postsharingbe/PostSharingBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gucardev.postsharingbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostSharingBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
