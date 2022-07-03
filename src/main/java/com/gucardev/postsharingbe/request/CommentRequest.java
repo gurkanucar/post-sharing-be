9f29348a8fbb7d0ad495c596166b19aec9e3fde3
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-07-03 22:34:56 +0300

    request models updated

--- src/main/java/com/gucardev/postsharingbe/request/CommentRequest.java
+++ src/main/java/com/gucardev/postsharingbe/request/CommentRequest.java
@@ -2,9 +2,11 @@ package com.gucardev.postsharingbe.request;
 
 import com.gucardev.postsharingbe.model.Comment;
 import com.gucardev.postsharingbe.model.Post;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class CommentRequest {
 
     private Post post;
--- src/main/java/com/gucardev/postsharingbe/request/LikeRequest.java
+++ src/main/java/com/gucardev/postsharingbe/request/LikeRequest.java
@@ -2,12 +2,15 @@ package com.gucardev.postsharingbe.request;
 
 import com.gucardev.postsharingbe.model.Post;
 import com.gucardev.postsharingbe.model.User;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class LikeRequest {
 
-    private User user;
     private Post post;
+    private User user;
+
 
 }
