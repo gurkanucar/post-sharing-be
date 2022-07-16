8cc35e1b727b55688725be6579fbc76a6d7f1ea1
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-07-16 20:35:30 +0300

    Notification models updates

--- src/main/java/com/gucardev/postsharingbe/model/Notification.java
+++ src/main/java/com/gucardev/postsharingbe/model/Notification.java
@@ -4,12 +4,27 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Document
 public class Notification {
-    private String message;
-    private NotificationType type;
+
+    @Id
+    private String id;
+
+    private String content;
+
+    private User userTo;
+
+    private User userFrom;
+
+    private NotificationType notificationType;
+
+    private boolean delivered;
+
 }
--- src/main/java/com/gucardev/postsharingbe/service/NotificationService.java
+++
@@ -1,9 +0,0 @@
-package com.gucardev.postsharingbe.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class NotificationService {
-
-
-}
